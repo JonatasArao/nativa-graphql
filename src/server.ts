4fa40a05ea3e52830fd61512d77e5c83a96cec6a
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-06-24 12:30:38 -0300

    feat: added morgan middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 import apiRoutes from '@/api/routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/', apiRoutes);
 
