27a9ec50af5abb9565681570b19b90232972d877
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-06-24 09:10:59 -0300

    feat: initial express structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Olá Nativa");
+import express from 'express'
+
+import { Router, Request, Response } from 'express';
+
+const app = express();
+
+const route = Router()
+
+const port = 3000
+
+app.use(express.json())
+
+route.get('/dica-do-dia', (req: Request, res: Response) => {
+  res.json({ dica: 'uma frase inspiradora da Nativa' })
+})
+
+app.use(route)
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`)
+})
