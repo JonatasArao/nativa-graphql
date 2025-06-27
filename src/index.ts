64c5752cd02654e088e77109a8d8a844b4bb7dea
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-06-27 08:29:47 -0300

    chore: updating README.md to gql_catalog project

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { config } from '@/config';
 async function main() {
   const app = await startServer();
   app.listen(config.port, () => {
-    console.log(`🚀 Servidor de dicas rodando em ${config.port}/dica-do-dia`);
+    console.log(`🚀 Servidor de dicas rodando em http://localhost:${config.port}/dica-do-dia`);
     console.log(`🚀 Servidor GraphQL rodando em http://localhost:${config.port}/graphql`);
   });
 }
