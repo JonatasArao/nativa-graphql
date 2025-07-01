9dc59e13c18fd1dde2af42591621c3e7e0a819f5
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-07-01 08:52:12 -0300

    feat: User Service added and implemented on resolver

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -3,18 +3,20 @@ import { LineService } from '@/services/line.service'
 import { ProductService } from '@/services/product.service'
 import { ProductParent, ProductFilterInput } from '@/models/product.model'
 import { LineFilterInput } from '@/models/line.model'
+import { UserService } from '@/services/user.service';
 
 const prisma = new PrismaClient();
 const lineService = new LineService(prisma);
 const productService = new ProductService(prisma);
+const userService = new UserService(prisma);
 
 export const resolvers = {
   Query: {
-    product: (_: any, args: { id: string }) => {
-      return productService.getProduct(args.id);
+    product: async (_: any, args: { id: string }) => {
+      return await productService.getProduct(args.id);
     },
-    products: (_: any, args: { filter?: ProductFilterInput }) => {
-      return productService.getProducts(args.filter || {});
+    products: async (_: any, args: { filter?: ProductFilterInput }) => {
+      return await productService.getProducts(args.filter || {});
     },
     line: async (_: any, args: { id: string }) => {
       return await lineService.getLine(args.id);
@@ -23,9 +25,14 @@ export const resolvers = {
       return await lineService.getLines(args.filter || {});
     }
   },
+  Mutation : {
+    registerUser: async (_: any, args: { name: string, email: string }) => {
+      return await userService.registerUser(args.name, args.email);
+    },
+  },
   Product: {
-    line: (parent: ProductParent) => {
-      return lineService.getLine(parent.lineId);
+    line: async (parent: ProductParent) => {
+      return await lineService.getLine(parent.lineId);
     },
   },
 };
